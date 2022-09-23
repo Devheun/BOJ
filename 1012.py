6b4db42d428baff05b1f72ac3d28cc9cb673f750
Devheun <dudwo5600@korea.ac.kr>
2022-09-23 07:29:45 +0000

    BOJ

--- 1012.py
+++ 1012.py
@@ -2,40 +2,40 @@
 
 from collections import deque
 
-dx=[-1, 1, 0, 0] 
-dy=[0, 0, -1, 1]
+dx=[0,0,-1,1] #동서남북
+dy=[1,-1,0,0]
 
-def bfs(x, y, cnt):
-    q = deque()
-    q.append((x, y))
-    dist[x][y] = cnt
-
+def bfs(x,y):
+    q=deque()
+    vis[x][y]=True # 방문처리
+    q.append((x,y))
+    
     while q:
-        x, y = q.popleft()
+        cur_x,cur_y=q.popleft()
         for k in range(4):
-            nx, ny = x+dx[k], y+dy[k]
-
+            nx=cur_x+dx[k]
+            ny=cur_y+dy[k]
             if 0<=nx<n and 0<=ny<m:
-                if board[nx][ny] == 1 and dist[nx][ny] == -1:
-                    q.append((nx, ny))
-                    dist[nx][ny] = cnt
+                if not vis[nx][ny] and a[nx][ny]:
+                    q.append((nx,ny))
+                    vis[nx][ny]=True
+    
 
+t=int(input())
 
-t = int(input())
 for _ in range(t):
-    cnt = 0
-    n, m, k = map(int, input().split())
-    board = [[0] * m for _ in range(n)]
-    dist = [[-1]*m for _ in range(n)]
-
+    m,n,k=map(int,input().split()) # m이 열, n이 행
+    a=[[0]*m for _ in range(n)]
+    vis=[[False]*m for _ in range(n)]
+    
     for _ in range(k):
-        i, j = map(int, input().split())
-        board[i][j] = 1
-
+        y,x=map(int,input().split())
+        a[x][y]=1
+    
+    cnt=0
     for i in range(n):
         for j in range(m):
-            if board[i][j] == 1 and dist[i][j] == -1:
-                cnt += 1
-                bfs(i, j, cnt)
-
+            if not vis[i][j] and a[i][j]:
+                cnt+=1
+                bfs(i,j)
     print(cnt)
