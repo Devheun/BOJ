8f853b690d1fb113068e11aeae364c917874dee3
Devheun <dudwo5600@korea.ac.kr>
2022-10-26 14:34:28 +0000

    BOJ

--- 15654.py
+++ 15654.py
@@ -1,13 +1,13 @@
 # N과 M (5)
 
-def dfs(num):
+def dfs():
     if len(arr)==m:
         print(' '.join(map(str,arr)))
         return
     for i in input_arr:
         if i not in arr:
             arr.append(i)
-            dfs(i)
+            dfs()
             arr.pop()
 
 n,m=map(int,input().split())
@@ -15,4 +15,4 @@ input_arr=list(map(int,input().split()))
 input_arr.sort()
 arr=[]
 
-dfs(input_arr[0])
+dfs()
